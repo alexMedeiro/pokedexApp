d9183d1f27ab6111eda67ee76021a9c9b4fc7897
alexMedeiro <60632987+alexMedeiro@users.noreply.github.com>
2020-02-17 22:21:21 -0300

    Adicionando lista dinamica 17/02
    removido o html dos pokemons
    criado o array com os pokemons
    Utilizando o for para exibir os dados do array

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,82 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public listaPokemons = [
+    
+    {
+      numero: '001',
+      nome: 'Bulbasaur',
+      tipos: ['Grass', 'Poison'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+    },
+
+    {
+      numero: '007',
+      nome: 'Squirtle',
+      tipos: ['Walter'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
+    },
+
+    {
+      numero: '004',
+      nome: 'Charmander',
+      tipos: ['Fire'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'
+    },
+
+    
+    {
+      numero: '025',
+      nome: 'Pikachu',
+      tipos: ['Electric'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'
+    },
+
+    {
+      numero: '006',
+      nome: 'Charizard',
+      tipos: ['Fire'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png'
+    },
+
+    {
+      numero: '011',
+      nome: 'Metapod',
+      tipos: ['Bug'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/011.png'
+    },
+
+    {
+      numero: '015',
+      nome: 'Beedril',
+      tipos: ['Bug'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/015.png'
+    },
+
+    {
+      numero: '052',
+      nome: 'Meowth',
+      tipos: ['Normal'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/052.png'
+    },
+
+    
+    {
+      numero: '064',
+      nome: 'Kadabra',
+      tipos: ['Psychic'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/064.png'
+    },
+
+    {
+      numero: '066',
+      nome: 'Machop',
+      tipos: ['Fighting'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/066.png'
+    },
+];
+
+public listaFiltrada =[];
   constructor() {}
 
 }
