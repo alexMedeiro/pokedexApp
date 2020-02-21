9a797df584723c9019c123c3c9e59ff31e14dd44
alexMedeiro <60632987+alexMedeiro@users.noreply.github.com>
2020-02-21 20:08:25 -0300

    21/02
    adicionado filtro

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -83,6 +83,37 @@ export class HomePage {
 ];
 
 public listaFiltrada =[];
-  constructor() {}
+  constructor() {
+    this.resetarLista();
+
+  }
+
+  private resetarLista(){
+    this.listaFiltrada = this.listaPokemons;
+  }
+
+  public buscarPokemon(evento:any){
+    let busca = evento.target.value;
+
+      this.resetarLista();
+
+      if(busca && busca.trim() != ''){
+        this.listaFiltrada = this.listaFiltrada.filter(dados =>{
+
+          if (dados.numero.toLowerCase(). indexOf(busca.toLowerCase()) > -1){
+            return true;
+          }
+
+          if (dados.nome.toLowerCase(). indexOf(busca.toLowerCase()) > -1){
+            return true;
+          }
+          return false;
+        });
+      }
+
+    
+
+  }
+
 
 }
